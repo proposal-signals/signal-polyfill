b8ee846e1f1f0a5e5c4ad6b19531c1a31e127adf
Gvozd <gvozdev.viktor@gmail.com>
2025-01-17 18:46:11 +0300

    fix assignment of subtypes (#7) (#42)

--- src/equality.ts
+++ src/equality.ts
@@ -7,9 +7,13 @@
  */
 
 /**
- * A comparison function which can determine if two values are equal.
+ * An interface representing a comparison strategy to determine if two values are equal.
+ *
+ * @template T The type of the values to be compared.
  */
-export type ValueEqualityFn<T> = (a: T, b: T) => boolean;
+export interface ValueEqualityComparer<T> {
+  equal(a: T, b: T): boolean;
+}
 
 /**
  * The default equality function used for `signal` and `computed`, which uses referential equality.
