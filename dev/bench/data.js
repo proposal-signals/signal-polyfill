6393a4edfb084a7f078103c9502e0d30b6644e74
github-action-benchmark <github@users.noreply.github.com>
2025-02-13 12:51:34 +0000

    add Benchmarks (customSmallerIsBetter) benchmark result for cc29d0b9c5513d77bcd97600284091d0e0929a7e

---
+++ dev/bench/data.js
@@ -0,0 +1,187 @@
+window.BENCHMARK_DATA = {
+  "lastUpdate": 1739451094075,
+  "repoUrl": "https://github.com/proposal-signals/signal-polyfill",
+  "entries": {
+    "Benchmarks": [
+      {
+        "commit": {
+          "author": {
+            "email": "david-emmanuel.divernois@amadeus.com",
+            "name": "divdavem",
+            "username": "divdavem"
+          },
+          "committer": {
+            "email": "noreply@github.com",
+            "name": "GitHub",
+            "username": "web-flow"
+          },
+          "distinct": true,
+          "id": "cc29d0b9c5513d77bcd97600284091d0e0929a7e",
+          "message": "Benchmarks from js-reactivity-benchmark in CI (#47)",
+          "timestamp": "2025-02-13T07:48:28-05:00",
+          "tree_id": "4812bb61d9710336eb0998add4c74955904268b1",
+          "url": "https://github.com/proposal-signals/signal-polyfill/commit/cc29d0b9c5513d77bcd97600284091d0e0929a7e"
+        },
+        "date": 1739451093277,
+        "tool": "customSmallerIsBetter",
+        "benches": [
+          {
+            "name": "avoidablePropagation",
+            "value": 1306.1417309999997,
+            "unit": "ms"
+          },
+          {
+            "name": "broadPropagation",
+            "value": 1488.5011689999992,
+            "unit": "ms"
+          },
+          {
+            "name": "deepPropagation",
+            "value": 430.8274989999991,
+            "unit": "ms"
+          },
+          {
+            "name": "diamond",
+            "value": 1300.6872839999996,
+            "unit": "ms"
+          },
+          {
+            "name": "mux",
+            "value": 906.0254339999956,
+            "unit": "ms"
+          },
+          {
+            "name": "repeatedObservers",
+            "value": 318.43372100000124,
+            "unit": "ms"
+          },
+          {
+            "name": "triangle",
+            "value": 432.40965600000345,
+            "unit": "ms"
+          },
+          {
+            "name": "unstable",
+            "value": 1008.1577469999902,
+            "unit": "ms"
+          },
+          {
+            "name": "molBench",
+            "value": 679.4504490000108,
+            "unit": "ms"
+          },
+          {
+            "name": "createDataSignals",
+            "value": 19.65995099999418,
+            "unit": "ms"
+          },
+          {
+            "name": "createComputations0to1",
+            "value": 7.451371000002837,
+            "unit": "ms"
+          },
+          {
+            "name": "createComputations1to1",
+            "value": 6.8271309999981895,
+            "unit": "ms"
+          },
+          {
+            "name": "createComputations2to1",
+            "value": 5.7340709999989485,
+            "unit": "ms"
+          },
+          {
+            "name": "createComputations4to1",
+            "value": 6.349377999998978,
+            "unit": "ms"
+          },
+          {
+            "name": "createComputations1000to1",
+            "value": 3.844678000008571,
+            "unit": "ms"
+          },
+          {
+            "name": "createComputations1to2",
+            "value": 13.90876799999387,
+            "unit": "ms"
+          },
+          {
+            "name": "createComputations1to4",
+            "value": 9.657027000008384,
+            "unit": "ms"
+          },
+          {
+            "name": "createComputations1to8",
+            "value": 9.213644999996177,
+            "unit": "ms"
+          },
+          {
+            "name": "createComputations1to1000",
+            "value": 8.773008999996819,
+            "unit": "ms"
+          },
+          {
+            "name": "updateComputations1to1",
+            "value": 25.039657999994233,
+            "unit": "ms"
+          },
+          {
+            "name": "updateComputations2to1",
+            "value": 13.112141000005067,
+            "unit": "ms"
+          },
+          {
+            "name": "updateComputations4to1",
+            "value": 7.61604000000807,
+            "unit": "ms"
+          },
+          {
+            "name": "updateComputations1000to1",
+            "value": 2.835295999990194,
+            "unit": "ms"
+          },
+          {
+            "name": "updateComputations1to2",
+            "value": 11.618734000003315,
+            "unit": "ms"
+          },
+          {
+            "name": "updateComputations1to4",
+            "value": 9.464514999999665,
+            "unit": "ms"
+          },
+          {
+            "name": "updateComputations1to1000",
+            "value": 2.391875000001164,
+            "unit": "ms"
+          },
+          {
+            "name": "10x5 - 2 sources - read 20% (simple component)",
+            "value": 1726.089804000003,
+            "unit": "ms"
+          },
+          {
+            "name": "10x10 - 6 sources - dynamic - read 20% (dynamic component)",
+            "value": 768.5124020000076,
+            "unit": "ms"
+          },
+          {
+            "name": "1000x12 - 4 sources - dynamic (large web app)",
+            "value": 15874.799971,
+            "unit": "ms"
+          },
+          {
+            "name": "1000x5 - 25 sources (wide dense)",
+            "value": 10251.229447999998,
+            "unit": "ms"
+          },
+          {
+            "name": "5x500 - 3 sources (deep)",
+            "value": 584.2835210000048,
+            "unit": "ms"
+          }
+        ]
+      }
+    ]
+  }
+}
